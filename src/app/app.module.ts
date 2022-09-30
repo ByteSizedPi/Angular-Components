0e41ec8ce6c3ef5015a96364b53bb9d150ee4434
Johan Venter <johann12venter@gmail.com>
2022-09-30 16:41:18 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
+import { CarouselModule } from './carousel/carousel.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SideNavModule } from './side-nav/side-nav.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, SideNavModule, CarouselModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/button/button.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonDirective } from './button.directive';
+
+
+
+@NgModule({
+  declarations: [
+    ButtonDirective
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ButtonModule { }
